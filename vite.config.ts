fb34e77495d0118cf41756d453057c979833acf4
pranav-at-pinhead <pranav@pinheadanalytics.com>
2025-07-31 11:52:04 +0530

    Revert "Test cases"
    This reverts commit eb9811cb25c4e60c3a1111e188d7d313f863f552.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { UserConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,9 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  test: {
-    environment: "jsdom",
-    setupFiles: "./tests/setup.ts",
-    globals: true,
-  },
-} as UserConfig & { test: any });
+});
